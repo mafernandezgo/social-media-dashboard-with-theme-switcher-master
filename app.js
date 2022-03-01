63852b224c371cf051428eabee8b61ae3382a3fc
Manuela <manue93_55@hotmail.com>
2022-03-01 10:27:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body")
+const switchToggle = document.querySelector(".switch-toggle")
+const toggle = document.querySelector(".toggle")
+const divRating = document.querySelector("div")
+
+
+switchToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark")
+})
+
---
+++ data.js
@@ -0,0 +1,24 @@
+const data =[
+    {
+        profile = "@nathanf",
+        followers = 1987,
+        status = 12
+    },
+    {
+        profile = "@nathanf",
+        followers = 1044,
+        status = 99
+    },
+
+    {
+        profile = "@realnathanf",
+        followers = 11000,
+        status = 1099
+    },
+    {
+        profile = "Nathan F",
+        followers = 8239,
+        status = 144
+    },
+    
+]
